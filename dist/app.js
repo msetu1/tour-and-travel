5675978cb1ccc3db482616584e141ff3349a99c4
msetu1 <msetu5763@gmail.com>
2024-12-11 01:22:31 +0600

    code update

--- dist/app.js
+++ dist/app.js
@@ -6,11 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-// parser 
+// parser
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 // application routes
-app.use('/api/tour');
+// app.use('/api/tour');
 app.get('/', (req, res) => {
     res.send('Server is running !!');
 });
