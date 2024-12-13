134fe32ad5ff911a6554b368ec2b18017a487ab4
msetu1 <msetu5763@gmail.com>
2024-12-13 18:03:55 +0600

    project update

--- src/module/booking/booking.service.ts
+++ src/module/booking/booking.service.ts
@@ -1,33 +1,50 @@
+import mongoose from 'mongoose';
 import { Tour } from '../tour/tour.model';
 import { IBooking } from './booking.interface';
 import { Booking } from './booking.model';
 
 // create booking
-const createBooking = async (playLoad: IBooking):Promise<IBooking> => {
-  const { tour, bookingSlots } = playLoad;
-  const requiredTour = await Tour.findById(tour);
+const createBooking = async (playLoad: IBooking): Promise<IBooking> => {
+  const session = await mongoose.startSession();
+  session.startTransaction()
+  try {
+    const { tour, bookingSlots } = playLoad;
+    const requiredTour = await Tour.findById(tour);
 
-  if (!requiredTour) {
-    throw new Error('Tour not found');
+    if (!requiredTour) {
+      throw new Error('Tour not found');
+    }
+
+    const totalPrice = requiredTour.price * bookingSlots;
+
+    playLoad.totalPrice = totalPrice;
+    playLoad.bookingStatus = 'Pending';
+
+    if (requiredTour.availableSeats < bookingSlots) {
+      throw new Error('Not enough seats available');
+    }
+
+    const booking = await Booking.create([playLoad], { session });
+
+    const updatedTour = await Tour.findByIdAndUpdate(
+      booking[0].tour,
+      { $inc: { availableSeats: -bookingSlots } },
+      { new: true, session },
+    );
+
+    if (!updatedTour) {
+      throw new Error('Failed to update tour');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return booking[0];
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
   }
-
-  const totalPrice = requiredTour.price * bookingSlots;
-  playLoad.totalPrice = totalPrice;
-  playLoad.bookingStatus = 'Pending';
-
-  if(requiredTour.availableSeats<bookingSlots){
-    throw new Error('Not enough seats available')
-  }
-
-  const booking =await Booking.create(playLoad)
-
-  const updatedTour =await Tour.findByIdAndUpdate(tour,{$inc:{availableSeats:-booking}},{new:true});
-
-  if (!updatedTour) {
-    throw new Error('Failed to update tour')
-  }
-
-  return booking;
 };
 
 export const BookingService = {
